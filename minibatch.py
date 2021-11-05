6def42b5ae13c6c7cd135283b1a6a790e4c0c5ab
akhmetov ildar <akhmetov.ih@yandex.ru>
2021-11-05 11:36:32 +0300

    first code

---
+++ minibatch.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.insert(0, 'F:\\projects\\python\\DeepLearning\\FDL')
+
+from fdl_examples.datatools import input_data
+mnist = input_data.read_data_sets("data/", one_hot=True)
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+import time, shutil, os
+
+def get_minibatch():
+    minibatch_x, minibatch_y = mnist.train.next_batch(batch_size)
+    return minibatch_x
+
+if __name__ == '__main__':
+    batch_size = 100
+    x = tf.placeholder(tf.float32,shape=[None,784],name="x")
+    W = tf.Variable(tf.random_uniform([784,10],-1,1),name = "W")
+    b = tf.Variable(tf.zeros([10]), name = "biases")
+    output = tf.matmul(x, W) + b
+    init_op = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init_op)
+    feed_dict_source = {x: get_minibatch()}
+    res = sess.run(output,feed_dict=feed_dict_source)
+    #print(res)
